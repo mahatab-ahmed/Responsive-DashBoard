7fbc7f1643afb6ebf010d632f1c090599a2fff18
Mahatab Ahmed <72667512+mahatab-ahmed@users.noreply.github.com>
2021-08-25 15:36:42 +0530

    Add files via upload

---
+++ dash.js
@@ -0,0 +1,10 @@
+const mas = document.getElementsByClassName('toggle');
+
+function togglemenu() {
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
